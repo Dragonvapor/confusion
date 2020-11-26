ddb5ab0cc68c62ad7b984f53e5b2842d1896e110
dragonvapor <abhisheknarve@gmail.com>
2020-11-26 15:33:20 +0530

    course complete

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { baseUrl } from '../shared/baseUrl'
-import { FadeTransform, Fade, Stagger } from 'react-animation-components'
+import { Fade, Stagger } from 'react-animation-components'
 
 
 const RenderLeader = (props) => {
@@ -97,4 +97,4 @@ function About(props) {
 
 
 
-export default About;  
+export default About; 
